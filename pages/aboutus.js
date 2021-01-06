f603480dc40a721af6d82a61d30d5148c5954a53
RandyLiu6410 <wert6410@gmail.com>
2021-01-06 22:34:25 +0800

    name

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -45,7 +45,7 @@ export default function LandingPage(props) {
       <div className={classes.container}>
         <GridContainer direction="row" alignItems="center" justify="flex-start" spacing={10} className={classes.gridContainer}>
           <GridItem xl={4} sm={4} xs={12}>
-            <MemberGrid no={1} name={"江佩儀"} image={require("assets/img/hci/peiyi_with_sign_3x.png")} degree={"BSc 2022, CS"} school={"Georgia Institute of Technology"}/>
+            <MemberGrid no={1} name={"江沛儀"} image={require("assets/img/hci/peiyi_with_sign_3x.png")} degree={"BSc 2022, CS"} school={"Georgia Institute of Technology"}/>
           </GridItem>
           <GridItem xl={4} sm={4} xs={12}>
             <MemberGrid no={2} name={"劉鎧禎"} image={require("assets/img/hci/randy_with_sign_3xpng.png")} degree={"MSc 2021, CAECE"} school={"National Taiwan University"}/>
